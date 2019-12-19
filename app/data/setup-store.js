3937ec3b3af31670a8ea1974fd88bdbf4226de60
Kyle Combes <kylecombes@gmail.com>
2019-12-18 20:10:57 -1000

    Convert reducers.js to reducers.ts.

--- app/data/setup-store.js
+++ app/data/setup-store.js
@@ -13,36 +13,6 @@ import {
 import * as reducers from './reducers';
 
 export default function () {
-  // Configure Redux
-  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent);
-  const initialState = {
-    boards: {
-      allBoards: {},
-      currentBoardId: null,
-    },
-    general: {
-      debug: window.debug,
-      isMobile,
-      focusedPersonId: null,
-      masonryLayoutTrigger: false,
-      server: window.SERVER_URI,
-    },
-    infoSidebar: {
-      sidebarType: null,
-      elementId: null,
-    },
-    navSidebar: {
-      visible: true,
-    },
-    people: null,
-    popup: {
-      type: null,
-      isClosable: true,
-    },
-    quotes: null,
-    user: null,
-  };
-
   const middleware = [
     thunkMiddleware.withExtraArgument({ emit }),
     // routeMiddleware,
@@ -51,7 +21,7 @@ export default function () {
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const store = createStore(
     combineReducers({ ...reducers }),
-    initialState,
+    {},
     // Only include the Redux devtools if they're installed and we're debugging
     (window.DEBUG && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)
       ? composeEnhancers(applyMiddleware(...middleware))
